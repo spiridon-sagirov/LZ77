a8d33a66cb54dec0cbe19147efde800452124b9d
tamarhrb <tamarhrb@gmail.com>
2022-07-14 17:28:48 +0300

    to_michal

--- LZ77.cpp
+++ LZ77.cpp
@@ -6,9 +6,9 @@
 #include <string>
 using namespace std;
 
-int Lz77(string sourseFile, int buffer) {
+int Lz77(string sourseFile,string destinationFile, int buffer) {
 	fstream my_file;
-	ofstream destination("destinationFile.Bin");
+	ofstream destination(destinationFile);
 	my_file.open(sourseFile, ios::in);
 	if (!my_file) 
 		return 0;
@@ -28,7 +28,14 @@ int Lz77(string sourseFile, int buffer) {
 	my_file.close();
 	return 0;
 }
+void shelllz77(string comand,string pathDecompress,string pathCompress,int buffer) {
+	if (comand._Equal("compress"))
+		Lz77(pathDecompress, pathCompress, buffer);
+	else if(comand._Equal("decompress"))
+		Lz77(pathCompress,pathDecompress, buffer);
+
+}
 int main() {
-	Lz77("binfile.Bin", 3);
+	shelllz77("compress", "image.png", "destinationFile.jpg",3);
 }
 
