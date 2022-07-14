0ea3dd8574bccdb469b0a779e2c5283edfc26292
michalJacobsohn <65172mj@gmail.com>
2022-07-14 16:12:48 +0300

    michal

--- LZ77.cpp
+++ LZ77.cpp
@@ -10,29 +10,24 @@ int Lz77(string sourseFile, int buffer) {
 	fstream my_file;
 	ofstream destination("destinationFile.txt");
 	my_file.open(sourseFile, ios::in);
-	if (!my_file) {
-		cout << "No such file";
-	}
-	else {
-		char ch;
-		while (!my_file.eof())
+	if (!my_file) 
+		return 0;
+	char ch;
+	while (!my_file.eof())
+	{
+		string res = "";
+		for (int i = 0; i < buffer; i++)
 		{
-			string res = "";
-			for (int i = 0; i < buffer; i++)
-			{
-				my_file >> noskipws >> ch;
-				if (my_file.eof())
-					break;
-				res += ch;
-			}
-			destination << res;
+			my_file >> noskipws >> ch;
+			if (my_file.eof())
+				break;
+			res += ch;
 		}
+		destination << res;
 	}
 	my_file.close();
 	return 0;
 }
-
-
 int main() {
 	Lz77("sourseFile.txt", 3);
 }
