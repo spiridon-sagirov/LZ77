0d291d29aa6513d4a1d597a4ed3ca1fb1288a632
Spiridon Sagirov <Spiridon.Sagirov@wdc.com>
2022-07-20 20:41:14 +0300

    Unit Tests can be written now

--- LZ77UnitTests/LZ77UnitTests.cpp
+++ LZ77UnitTests/LZ77UnitTests.cpp
@@ -3,37 +3,11 @@
 
 #include "stdafx.h"
 
-
-#include "..\LZ77Lib\Triplate.h"
-#include "..\LZ77Lib\TriplateFile.h"
-
-
-void free_test_function()
-{
-	//fstream f1;
-	//f1.open("pppppppppp", std::ios_base::app);
-	//if (!f1)
-	//{
-	//	cout << "File creation failed";
-	//}
-	////	f1 << "hello";
-	//char  num;
-	//f1 >> num;
-	//cout << "The next number is " << num;
-	//f1.close();
-	///*Triplate t1(21, 12, 'g');
-	//TriplateFile tf;
-	//tf.Open("qwertyuiop");
-	//tf.Write(t1);
-	//tf.Close();
-	//cout << tf.EndOfFile();*/
-
-	BOOST_TEST(true /* test assertion */);
-}
+#include "TripletFileTests.h"
 
 
 test_suite* init_unit_test_suite(int /*argc*/, char* /*argv*/[])
 {
-	framework::master_test_suite().add(BOOST_TEST_CASE(&free_test_function));
+	framework::master_test_suite().add(BOOST_TEST_CASE(&triplet_test_sample));
 	return 0;
 }
