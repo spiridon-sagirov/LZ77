e813a38f81f0cc0490c4fb93f4834144944664ef
tamarhrb <tamarhrb@gmail.com>
2022-07-27 16:55:09 +0300

    tamatPush

--- LZ77Test.py
+++ LZ77Test.py
@@ -2,41 +2,44 @@ import string
 import subprocess
 import sys
 import time
-import  os,csv
+import os,csv
 
-#1, Run the LZ77.exe (or LZ77_fake.cmd at the beginning) to compress the input file
+# Run the LZ77.exe to compress the input file
 def run(OriginalFile,compressFile,decompressFile):
     start_time_compress=time.time()
     subprocess.run([r"C:\westernDigitalBootcamp\LZ77\Debug\LZ77.exe", "compress", OriginalFile, compressFile, "5"])
-    calaulateTime(start_time_compress)
-    calaulateSizePercent(inputFile, outputFile, action)
 
-    #start_time_decompress = time.time();
-   # subprocess.run([r"C:\westernDigitalBootcamp\LZ77\Debug\LZ77.exe", "decompress", compressFile, decompressFile, "5"])
-
-    calaulateSize()
+    ctc=calaulateTime(start_time_compress)
+    ctp=calaulateSizePercent(OriginalFile, compressFile)
+    start_time_decompress = time.time()
+    time.sleep(1.0)
+    subprocess.run([r"C:\westernDigitalBootcamp\LZ77\lz77_fake.cmd", "decompress", compressFile, decompressFile, "5"])
+    ctd=calaulateTime(start_time_decompress)
+    cf=compareOfileAndDfile(OriginalFile, decompressFile)
+    writeToClv(OriginalFile, ctc, ctp, ctd, cf)
 
 # Calculate the time spent in the compression/decompression
 def calaulateTime(startTime):
     total_compress_time=time.time()-startTime
     return total_compress_time
 
-# 3. Calculate the size of the compressed file
-#6. Compare the decompressed file with the original one
-def calaulateSizePercent(inputFile,outputFile,action):
+# Calculate the size of the compressed file
+def calaulateSizePercent(inputFile,outputFile):
     sizeOutputFile = os.stat(outputFile).st_size
     sizeInputFile=os.stat(inputFile).st_size
-    return (sizeInputFile-sizeCompressFile)/100
+    return (sizeInputFile-sizeOutputFile)/100
 
-#write the test output to excel file
+# compare the originalFile and the decompressFile
+def compareOfileAndDfile(OriginalFile,decompressFile):
+    sizeOriginalFile = os.stat(OriginalFile).st_size
+    sizedecompressFile = os.stat(decompressFile).st_size
+    return sizeOriginalFile == sizedecompressFile
 
-def writeToClv(OriginalFile,compressFile,decompressFile):
-    run(OriginalFile, compressFile, action="compress")
-    run(compressFile, decompressFile, action="decompress")
-    originalFileSize=calaulateSize(OriginalFile,compressFile,action="compress")
-    data=[OriginalFile,(sizeDecompressFile-sizeCompressFile)/100,total_compress_time,total_decompress_time,'tamarr']
+# write the test output to excel file
+def writeToClv(OriginalFile,ctc,ctp,ctd,cf):
+    data=[OriginalFile,os.stat(OriginalFile).st_size,ctp,ctc,ctd,cf]
     with open('TestOutput.csv', 'a',newline="") as f:
         writer = csv.writer(f)
         writer.writerow(data)
 
-
+run("source.txt","sourceCompress.txt","sourceDecompress.txt")
